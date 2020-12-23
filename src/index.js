631d23d5b0c37cad9bd06734942f062115fb77e8
Kaushik Kumaran <kumarankaushik@gmail.com>
2020-12-23 21:46:54 +0400

    Initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = (props) => {
+
+  const [count, setCount] = useState(props.count)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const reset = () => {
+    setCount((props.count))
+  }
+
+    return(
+      <div>
+       <p>Current Count: {count}</p>
+       <button onClick={increment}>Add</button>
+       <button onClick={decrement}>Subtract</button>
+       <button onClick={reset}>Reset</button>
+      </div>
+    )
+}
+
+App.defaultProps = {
+  count: 0
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
